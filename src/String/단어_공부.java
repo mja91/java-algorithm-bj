61f10e9a1a5d2475097327a2f8457a929a055445
Nash <park90072001@gmail.com>
2024-04-17 00:08:18 +0900

    feat: 백준 단어 공부 풀이

---
+++ src/String/단어_공부.java
@@ -0,0 +1,34 @@
+package String;
+
+import java.util.Scanner;
+
+public class 단어_공부 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[26];
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        for (int i=0; i<input.length(); i++) {
+            char s = input.charAt(i);
+            if ('A' <= s && s <= 'Z') {
+                arr[s - 'A']++;
+            } else {
+                arr[s - 'a']++;
+            }
+        }
+
+        int max = -1;
+        char chr = '?';
+        for (int i=0; i<26; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                chr = (char) (i + 'A');
+            } else if (max == arr[i]) {
+                chr = '?';
+            }
+        }
+        System.out.println(chr);
+    }
+}
