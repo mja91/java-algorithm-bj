baf3ac5348a85892cd51f31c8f4f98d8bc773ba7
Nash <park90072001@gmail.com>
2024-04-21 00:19:52 +0900

    add: 백준 8진수, 10진수, 16진수 문제 풀이

---
+++ src/String/진수_문자열.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Scanner;
+
+public class 진수_문자열 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String str = sc.next();
+        if (str.startsWith("0x")) {
+            System.out.println(Integer.parseInt(str.substring(2), 16));
+        } else if (str.startsWith("0")) {
+            System.out.println(Integer.parseInt(str, 8));
+        } else {
+            System.out.println(str);
+        }
+    }
+}
