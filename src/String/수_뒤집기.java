44c6b1909b92cc840917bbd64f16045a858cf380
Nash <park90072001@gmail.com>
2024-04-20 22:07:38 +0900

    feat: 백준 수 뒤집기 풀이

---
+++ src/String/수_뒤집기.java
@@ -0,0 +1,38 @@
+package String;
+
+import java.util.Scanner;
+
+public class 수_뒤집기 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int firstNum = sc.nextInt();
+            String reverseStr = new StringBuilder(Integer.toString(firstNum)).reverse().toString();
+            int secondNum = Integer.parseInt(reverseStr);
+            int resultNum = firstNum + secondNum;
+            String resultStr = Integer.toString(resultNum);
+
+            if (check(resultStr)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    public static boolean check(String str) {
+        int left = 0;
+        int right = str.length()-1;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
