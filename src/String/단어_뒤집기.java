119ee9819045f40c34c51d74b45019861648d270
Nash <park90072001@gmail.com>
2024-04-21 22:42:41 +0900

    update: 3차 for문 조건 변경

---
+++ src/String/단어_뒤집기.java
@@ -0,0 +1,32 @@
+package String;
+
+import java.util.Scanner;
+
+public class 단어_뒤집기 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt(sc.nextLine());
+        for (int i=0; i<num; i++) {
+            String str = sc.nextLine();
+            StringBuilder sb = new StringBuilder();
+            String[] strArr = str.split(" ");
+            for (int j=0; j<strArr.length; j++) {
+                String now = strArr[j];
+                String nowResult = "";
+                char[] charArr = now.toCharArray();
+                for (int k=charArr.length-1; k>=0; k--) {
+                    nowResult += charArr[k];
+                }
+                sb.append(nowResult).append(" ");
+            }
+
+            String result = sb.toString();
+            result = result.substring(0, result.length()-1);
+            System.out.println(result);
+        }
+
+        sc.close();
+    }
+}
