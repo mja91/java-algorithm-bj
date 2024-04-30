23b16cc316d30eb6c5f0953a65dea780f41dc46b
Nash <park90072001@gmail.com>
2024-04-30 21:53:12 +0900

    add: 백준 문자열 문제 풀이

---
+++ src/String/문자열.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 문자열 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        String A = arr[0];
+        String B = arr[1];
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int start = 0; start <= B.length() - A.length(); start++) {
+            int diff = 0;
+            for (int i = 0; i < A.length(); i++) {
+                if (A.charAt(i) != B.charAt(start + i)) {
+                    diff++;
+                }
+            }
+
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+
+        System.out.println(minDiff);
+
+        br.close();
+    }
+}
