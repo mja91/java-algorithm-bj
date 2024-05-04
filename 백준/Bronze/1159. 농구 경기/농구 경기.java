9688f3663df1f06c34844bae03be90ffa3021053
Nash <67402799+park900720000@users.noreply.github.com>
2024-05-05 00:10:00 +0900

    [Bronze II] Title: 농구 경기, Time: 212 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num = Integer.parseInt(sc.nextLine());
+        int[] arr = new int[26];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < num; i++) {
+            String name = sc.nextLine();
+            char fc = name.charAt(0);
+            int fcn = fc - 'a';
+            arr[fcn]++;
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] >= 5) {
+                sb.append((char) (j + 'a'));
+            }
+        }
+
+        if (!"".equals(sb.toString())) {
+            System.out.println(sb);
+        } else {
+            System.out.println("PREDAJA");
+        }
+
+        sc.close();
+    }
+}
