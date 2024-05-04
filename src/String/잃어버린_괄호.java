43636eb41ef75003e58a34694bc9e0d44879caa0
Nash <park90072001@gmail.com>
2024-05-04 23:53:25 +0900

    add: 백준 잃어버린 괄호 문제 풀이

---
+++ src/String/잃어버린_괄호.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 잃어버린_괄호 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text = br.readLine();
+        solve(text);
+        br.close();
+    }
+
+    private static void solve(String text) {
+        String[] arr = text.split("-");
+
+        int minResult = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (String num : arr[i].split("\\+")) {
+                if (i == 0) {
+                    minResult += Integer.parseInt(num);
+                } else {
+                    minResult -= Integer.parseInt(num);
+                }
+            }
+        }
+
+        System.out.println(minResult);
+    }
+}
