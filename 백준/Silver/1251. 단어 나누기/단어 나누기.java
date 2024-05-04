0311485163c6647729319392edf047396aa0309c
Nash <67402799+park900720000@users.noreply.github.com>
2024-05-05 00:15:08 +0900

    [Silver V] Title: 단어 나누기, Time: 172 ms, Memory: 17048 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String result = solve(input);
+        System.out.println(result);
+        br.close();
+    }
+
+    private static String solve(String input) {
+        int length = input.length();
+        String minWord = null;
+
+        for (int i=1; i<length; i++) {
+            for (int j=i+1; j<length; j++) {
+                String part1 = new StringBuilder(input.substring(0, i)).reverse().toString();
+                String part2 = new StringBuilder(input.substring(i, j)).reverse().toString();
+                String part3 = new StringBuilder(input.substring(j)).reverse().toString();
+
+                String combined = part1 + part2 + part3;
+
+                if (minWord == null || combined.compareTo(minWord) < 0) {
+                    minWord = combined;
+                }
+            }
+        }
+
+        return minWord;
+    }
+}
